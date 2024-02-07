af40dc0b33abfacc843e59789776a267c0b0146f
Abdi Mussa <abdimussa87@gmail.com>
2024-02-07 22:14:16 +0300

    fix: updated hardhat config solidity version to match with casino

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.4.20",
   networks: {
     sepolia: {
       url: process.env.ALCHEMY_TESTNET_RPC_URL,
